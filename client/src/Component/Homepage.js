44edabf94f0d941bee8f970aa210bbe98cae67ab
Glenn Nguyo <glennnguyo1000@gmail.com>
2022-10-13 20:21:32 +0300

    CU-3p6dvpz - home page

--- client/src/Component/Homepage.js
+++ client/src/Component/Homepage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Slider from 'infinite-react-carousel'
 import '../Components/style.css'
 import { Container } from 'reactstrap'
 
@@ -15,21 +14,11 @@ class  Homepage extends React.Component{
     const settings =  {
       autoplay: true,
       dots: true,
-      // duration: 100,
       initialSlide: false,
-      // infinite: true,
     };
     return (
         <Container >
       <div>
-        <Slider {...settings} >
-            <div><img src={require('../images/Carousal 1.jpg')} alt=""/></div>
-            <div><img src={require('../images/Carousal 2.jpg')} alt="Credit to Alisa Anton on Unsplash"/></div>
-            <div><img src={require('../images/Carousal 3.jpg')} alt="Credit to Igor Ovsyannykov on Unsplash"/></div>
-            <div><img src={require('../images/Carousal 4.jpg')} alt="Credit to Pierre Châtel-Innocenti on Unsplash"/></div>
-            <div><img src={require('../images/Carousal 1.jpg')} alt="Credit to Richard Nolan on Unsplash"/></div>
-            <div><img src={require('../images/Carousal 3.jpg')} alt="Credit to Cristina Gottardi on Unsplash"/></div>
-        </Slider>
       </div>
       <br/><br/>
         </Container>
