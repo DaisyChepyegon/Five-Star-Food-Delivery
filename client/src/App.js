7b8aad2944282fcc5270fd9f744f77cb898d5838
DaisyChepyegon <daisychepyegon@gmail.com>
2022-10-14 13:37:03 +0300

    create rating and review form

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css"
 import Home from './Components/home/Home';
+import Reviews from "./Components/Reviews/Reviews";
 function App() {
   return (
     <div className="App">
       <Home />
+      <Reviews/>
     </div>
   );
 }
