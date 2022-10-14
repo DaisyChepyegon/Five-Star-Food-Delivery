f6d3daaf16eb2c9901737b389e9de80ec31e7d48
JM <106306519+joshuamwale@users.noreply.github.com>
2022-10-14 10:44:26 +0300

    Revert "Task2/cu 3pc6jf1 login and signup frontend joshua mwale"

--- client/src/components/Welcome.js
+++
@@ -1,9 +0,0 @@
-function Welcome({ customer }) {
-    if (customer) {
-        return <h1>Welcome, {customer.first_name}!</h1>;
-    } else {
-        return <h1>Please Sign Up or Login</h1>;
-    }
-}
-
-export default Welcome;
